768933adb421180cbe3fc7f90d319fdbb824edc3
topdog27 <ahilton@byupathway.edu>
2025-03-13 12:35:44 -0600

    new file:   week02/Resumes/Job.cs modified:   week02/Resumes/Program.cs new file:   week02/Resumes/Resume.cs

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Package and Labeler";
+        job1._company = "UPS";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Police Officer";
+        job2._company = "NYPD";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+
+        Resume myresume = new Resume();
+        myresume._name = "Andrew Hilton";
+        myresume._jobs.Add(job1);
+        myresume._jobs.Add(job2);
+        myresume.Display();
+
+
+
+      
+
     }
 }
