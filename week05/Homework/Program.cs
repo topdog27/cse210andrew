eb405cc7a811c4efdf07d0f6bd1078a4fda3acf3
topdog27 <ahilton@byupathway.edu>
2025-04-14 13:34:03 -0600

    new file:   week05/Homework/Assignment.cs 	new file:   week05/Homework/MathAssignment.cs 	modified:   week05/Homework/Program.cs 	new file:   week05/Homework/WritingAssignment.cs

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Andrew Hilton", "Learning Activity: Iheritance");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Andrew", "Multiplication", "1.1", "7-8");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Samulel", "English", "Essay: To Kill a Mocking Bird");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
